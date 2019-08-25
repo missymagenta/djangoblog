cfc6c837a438d0d4190c4baa7da92428b9c19662
missymagenta <alina.soare37@gmail.com>
2019-08-25 18:03:47 +0200

    finished views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView, ListView, DetailView, CreateView)
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 from blog.models import Post, Comment
 from blog.forms import import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 class AboutView(TemplateView):
@@ -23,3 +24,25 @@ class CreatePostView(LoginRequiredMixin,CreateView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    login_url = '/login/'
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_draft_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+
